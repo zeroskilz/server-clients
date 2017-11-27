4d4f5e991c302da46d0a84f8c7f4c2d3e06e9d23
zeroskilz <zeroskilz@mail.com>
2017-11-27 16:02:17 -0500

    added a function that acts on keyword to jump to a file download function that essentially just streams data into a file

--- client/client.py
+++ client/client.py
@@ -1,14 +1,46 @@
+#!/usr/bin/python
 import socket
 
-port = 4201
-host = '127.0.0.1'
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-soc.connect((host,port))
+def socketConnect():
+    global soc
+    global data
+    port = 4201
+    host = '127.0.0.1'
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.connect((host,port))
 
-while True:
+
+def sendData():
     buf = raw_input("Send Buffer : ")
-    str(buf)
+    buf = str(buf)
     pay = soc.send(buf)
     data = soc.recv(4096)
+    if 'file' in buf:
+        print("caught keyword activating fileSave function")
+        return fileSave(data)
 
-    soc.close()
+def fileSave(data): # accept argument of file path
+    fd = open('testFile.tmp', 'w') # open file for writting
+    while True:  # loop to catch data
+        print("data in transmission")
+        data = soc.recv(4096)
+        print("data = %s : " %data)
+        if not data:
+            break
+        fd.write(data)
+    fd.close()
+    print("File Transfer Complete exiting function:")
+
+socketConnect()
+while (1):
+    sendData()
+
+
+
+
+
+
+
+
+
+
